29c47c1091e1b4d13516367b790453a80c6fa849
DarQxxx <norbertju@gmail.com>
2022-01-02 20:35:47 +0100

    added partly rwd to movie page and header

--- src/Header.js
+++ src/Header.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import "./Header.css"
 import "./Bootstrap.css"
-import {FaSignInAlt, FaFacebook, FaSearch} from "react-icons/fa";
-function Header({scrollHeight}) {
+import {FaSignInAlt, FaFacebook, FaSearch, FaBars} from "react-icons/fa";
+import { Link } from 'react-router-dom';
+function Header() {
     return (
         <div className={`header`} >
             <div className="header__logo">
-            MOVIELIST</div>
-            <div className="header__input ml-25px"><div className="header__inputIcon"><FaSearch/></div><input  type="text" placeholder='Szukaj filmów, których pożądasz' /></div>
-            <div className="header__fbLog ml-25px"><div className="header__fbLogIcon"><FaFacebook/></div>Zaloguj się kontem Facebook</div>
-            <div className="header__Log ml-10px"><div className="header__LogIcon"><FaSignInAlt/></div>Zaloguj się</div>
+            <Link to="/">MOVIELIST</Link></div>
+            <div className="header__input ml-25px header__rwd--hide"><div className="header__inputIcon"><FaSearch/></div><input  type="text" placeholder='Szukaj filmów, których pożądasz' /></div>
+            <div className="header__fbLog ml-25px header__rwd--hide"><div className="header__fbLogIcon"><FaFacebook/></div>Zaloguj się kontem Facebook</div>
+            <div className="header__Log ml-10px header__rwd--hide"><div className="header__LogIcon"><FaSignInAlt/></div>Zaloguj się</div>
+            <div className="header__iconsRwd header__rwd--show">
+                <div className="header__iconsRwdLog"><FaSignInAlt/></div>
+                <div className="header__iconsRwdBurger"><FaBars/></div>
+                </div>
 
         </div>
     )
